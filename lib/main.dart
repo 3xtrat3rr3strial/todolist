ab5ae4c7b9c4826d7fad346581a451d83a2d995e
3xtrat3rr3strial <s.komarova99@gmail.com>
2023-05-29 09:22:06 +0200

    lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,80 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+class Todo {
+  Todo({required this.name, required this.checked});
+  final String name;
+  bool checked;
 }
 
+void main() => runApp(const MyApp());
+
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'To-Do List App',
-      home: TodoList(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(
+        title: 'To-do List App',
+        todos: [],
+      ),
     );
   }
 }
 
-class TodoList extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title, required this.todos})
+      : super(key: key);
+
+  final String title;
+  final List<Todo> todos;
+
   @override
-  _TodoListState createState() => _TodoListState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _TodoListState extends State<TodoList> {
-  final List<String> _items = [];
+class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _textEditingController = TextEditingController();
 
-  void _addItem(String item) {
+  void _addTodo() {
+    String text = _textEditingController.text;
+    if (text.isNotEmpty) {
+      setState(() {
+        widget.todos.add(Todo(name: text, checked: false));
+      });
+      _textEditingController.clear();
+    }
+  }
+
+  void _removeTodoAtIndex(int index) {
     setState(() {
-      _items.add(item);
+      widget.todos.removeAt(index);
     });
   }
 
-  void _removeItem(int index) {
+  void _toggleTodoAtIndex(int index) {
     setState(() {
-      _items.removeAt(index);
+      widget.todos[index].checked = !widget.todos[index].checked;
     });
   }
 
-  Widget _buildItem(String item, int index) {
+  Widget _buildTodoItem(BuildContext context, int index) {
+    final todo = widget.todos[index];
+
     return ListTile(
-      title: Text(item),
+      leading: Checkbox(
+        value: todo.checked,
+        onChanged: (_) => _toggleTodoAtIndex(index),
+      ),
+      title: Text(todo.name),
       trailing: IconButton(
-        icon: Icon(Icons.delete),
-        onPressed: () => _removeItem(index),
+        icon: const Icon(Icons.delete),
+        onPressed: () => _removeTodoAtIndex(index),
       ),
     );
   }
@@ -48,41 +83,44 @@ class _TodoListState extends State<TodoList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('To-Do List'),
+        title: Text(widget.title),
       ),
-      body: ListView.builder(
-        itemCount: _items.length,
-        itemBuilder: (BuildContext context, int index) {
-          return _buildItem(_items[index], index);
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () async {
-          final String? item = await showDialog<String>(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: Text('Add Item'),
-                content: TextField(),
-                actions: [
-                  TextButton(
-                    child: Text('Cancel'),
-                    onPressed: () => Navigator.of(context).pop(),
-                  ),
-                  TextButton(
-                    child: Text('Add'),
-                    onPressed: () => Navigator.of(context).pop(),
-                  ),
-                ],
-              );
+      body: Column(
+        children: [
+          TextField(
+            controller: _textEditingController,
+            decoration: const InputDecoration(
+              hintText: 'Enter a todo',
+              contentPadding: EdgeInsets.all(8.0),
+            ),
+            onSubmitted: (_) {
+              _addTodo();
+              setState(() {});
             },
-          );
-
-          if (item != null && item.isNotEmpty) {
-            _addItem(item);
-          }
-        },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.todos.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Dismissible(
+                  key: UniqueKey(),
+                  onDismissed: (direction) {
+                    setState(() {
+                      widget.todos.removeAt(index);
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Task removed"),
+                        duration: const Duration(seconds: 2),
+                      ),
+                    );
+                  },
+                  child: _buildTodoItem(context, index),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
