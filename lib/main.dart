ad133113979a5a5fdb1a2cb2783ab659da69ac61
3xtrat3rr3strial <s.komarova99@gmail.com>
2023-05-29 10:34:10 +0200

    buttons: add, check all, delete all

--- lib/main.dart
+++ lib/main.dart
@@ -63,6 +63,20 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _checkAllTodos() {
+    setState(() {
+      for (var todo in widget.todos) {
+        todo.checked = true;
+      }
+    });
+  }
+
+  void _deleteAllTodos() {
+    setState(() {
+      widget.todos.clear();
+    });
+  }
+
   Widget _buildTodoItem(BuildContext context, int index) {
     final todo = widget.todos[index];
 
@@ -86,37 +100,54 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          TextField(
-            controller: _textEditingController,
-            decoration: const InputDecoration(
-              hintText: 'Enter a todo',
-              contentPadding: EdgeInsets.all(8.0),
-            ),
-            onSubmitted: (_) {
-              _addTodo();
-              setState(() {});
-            },
+          Row(
+            textDirection: TextDirection.rtl,
+            children: [
+              ElevatedButton(
+                onPressed: _addTodo,
+                child: const Text('Add'),
+              ),
+              Expanded(
+                child: TextField(
+                  controller: _textEditingController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter a todo',
+                    contentPadding: EdgeInsets.all(8.0),
+                  ),
+                  onSubmitted: (_) {
+                    _addTodo();
+                    setState(() {});
+                  },
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _checkAllTodos();
+                  setState(() {});
+                },
+                child: const Text('Check All'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _deleteAllTodos();
+                  setState(() {});
+                },
+                child: const Text('Delete All'),
+              ),
+            ],
           ),
           Expanded(
             child: ListView.builder(
               itemCount: widget.todos.length,
               itemBuilder: (BuildContext context, int index) {
-                return Dismissible(
-                  key: UniqueKey(),
-                  onDismissed: (direction) {
-                    setState(() {
-                      widget.todos.removeAt(index);
-                    });
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text("Task removed"),
-                        duration: const Duration(seconds: 2),
-                      ),
-                    );
-                  },
-                  child: _buildTodoItem(context, index),
-                );
+                return _buildTodoItem(context, index);
               },
             ),
           ),
